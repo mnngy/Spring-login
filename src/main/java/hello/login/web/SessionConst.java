430024973e90dffb95125ccfeaa8e0e99345378f
mnngy <59257197+mnngy@users.noreply.github.com>
2021-12-27 18:15:47 +0900

    로그인 처리하기 - 서블릿 HTTP 세션 1

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,15 @@
+package hello.login.web;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
+
+/*
+public interface SessionConstV1 {
+    String LOGIN_MEMBER = "loginMember";
+}
+
+public abstract class SessionConstV2 {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
+ */
